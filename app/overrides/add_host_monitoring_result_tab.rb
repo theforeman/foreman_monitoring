b8a794a0ac34be2d29a471c42a2d01e362468917
Dirk Goetz <dirk.goetz@netways.de>
2019-05-27 08:14:27 +0200

    fix rubocop hash alignment

--- app/overrides/add_host_monitoring_result_tab.rb
+++ app/overrides/add_host_monitoring_result_tab.rb
@@ -1,9 +1,9 @@
-Deface::Override.new(:virtual_path  => 'hosts/show',
-                     :name          => 'add_monitoring_result_tab',
+Deface::Override.new(:virtual_path => 'hosts/show',
+                     :name => 'add_monitoring_result_tab',
                      :insert_bottom => 'ul.nav-tabs',
-                     :partial       => 'monitoring_results/host_tab')
+                     :partial => 'monitoring_results/host_tab')
 
-Deface::Override.new(:virtual_path  => 'hosts/show',
-                     :name          => 'add_monitoring_result_tab_pane',
+Deface::Override.new(:virtual_path => 'hosts/show',
+                     :name => 'add_monitoring_result_tab_pane',
                      :insert_bottom => 'div.tab-content',
-                     :partial       => 'monitoring_results/host_tab_pane')
+                     :partial => 'monitoring_results/host_tab_pane')
--- app/overrides/add_host_multiple_power_set_downtime_checkbox.rb
+++ app/overrides/add_host_multiple_power_set_downtime_checkbox.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path  => 'hosts/select_multiple_power_state',
-                     :name          => 'add_host_multiple_power_set_downtime_checkbox',
+Deface::Override.new(:virtual_path => 'hosts/select_multiple_power_state',
+                     :name => 'add_host_multiple_power_set_downtime_checkbox',
                      :insert_before => "erb[silent]:contains('end')",
-                     :partial       => 'hosts/host_downtime_checkbox')
+                     :partial => 'hosts/host_downtime_checkbox')
