theforeman/foreman_monitoring

commit 021ecc26654d2a7d21a8a60ebd9ff53b7db15c26

migrate to use Settings DSL
Fixes: #82
Fixes: #81

lib/foreman_monitoring/engine.rb
@@ -18,19 +18,31 @@ module ForemanMonitoring
       end
     end
 
-    initializer 'foreman_monitoring.load_default_settings',
-                :before => :load_config_initializers do |_app|
-      setting_table_exists = begin
-                               Setting.table_exists?
-                             rescue StandardError
-                               false
-                             end
-      require_dependency File.expand_path('../../app/models/setting/monitoring.rb', __dir__) if setting_table_exists
-    end
-
     initializer 'foreman_monitoring.register_plugin', :before => :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_monitoring do
-        requires_foreman '>= 2.2'
+        requires_foreman '>= 3.0'
+
+        settings do
+          category(:monitoring, N_('Monitoring')) do
+            setting('monitoring_affect_global_status',
+                    type: :boolean,
+                    description: N_("Monitoring status will affect a host's global status when enabled"),
+                    default: true,
+                    full_name: N_('Monitoring status should affect global status'))
+            setting('monitoring_create_action',
+                    type: :string,
+                    description: N_('What action should be taken when a host is created'),
+                    default: 'create',
+                    full_name: N_('Host Create Action'),
+                    collection: ::Monitoring::CREATE_ACTIONS)
+            setting('monitoring_delete_action',
+                    type: :string,
+                    description: N_('What action should be taken when a host is deleted'),
+                    default: 'delete',
+                    full_name: N_('Host Delete Action'),
+                    collection: ::Monitoring::DELETE_ACTIONS)
+          end
+        end
 
         apipie_documented_controllers ["#{ForemanMonitoring::Engine.root}/app/controllers/api/v2/*.rb"]
 

app/models/setting/monitoring.rb
@@ -1,34 +1,0 @@
-# frozen_string_literal: true
-
-class Setting
-  class Monitoring < ::Setting
-    def self.default_settings
-      [
-        set('monitoring_affect_global_status',
-            _("Monitoring status will affect a host's global status when enabled"),
-            true, N_('Monitoring status should affect global status')),
-        set('monitoring_create_action',
-            _('What action should be taken when a host is created'),
-            'create', N_('Host Create Action'), nil, :collection => proc { ::Monitoring::CREATE_ACTIONS }),
-        set('monitoring_delete_action',
-            _('What action should be taken when a host is deleted'),
-            'delete', N_('Host Delete Action'), nil, :collection => proc { ::Monitoring::DELETE_ACTIONS })
-      ]
-    end
-
-    def self.load_defaults
-      # Check the table exists
-      return unless super
-
-      self.transaction do
-        default_settings.each { |s| self.create! s.update(:category => 'Setting::Monitoring') }
-      end
-
-      true
-    end
-
-    def self.humanized_category
-      N_('Monitoring')
-    end
-  end
-end

db/migrate/20221026065954_fix_monitoring_settings_category_to_dsl.rb
@@ -1,0 +1,9 @@
+# frozen_string_literal: true
+
+class FixMonitoringSettingsCategoryToDsl < ActiveRecord::Migration[6.0]
+  def up
+    # rubocop:disable Rails/SkipsModelValidations
+    Setting.where(category: 'Setting::Monitoring').update_all(category: 'Setting')
+    # rubocop:enable Rails/SkipsModelValidations
+  end
+end

test/test_plugin_helper.rb
@@ -11,10 +11,6 @@ FactoryBot.reload
 # Foreman's setup doesn't handle cleaning up for Minitest::Spec
 DatabaseCleaner.strategy = :transaction
 
-def setup_settings
-  Setting::Monitoring.load_defaults
-end
-
 def disable_monitoring_orchestration
   ProxyAPI::Monitoring.any_instance.stubs(:query_host).returns({})
 end

test/unit/host_status/monitoring_status_test.rb
@@ -4,7 +4,6 @@ require 'test_plugin_helper'
 
 class MonitoringStatusTest < ActiveSupport::TestCase
   setup do
-    setup_settings
     disable_monitoring_orchestration
   end
 

test/unit/host_test.rb
@@ -5,7 +5,6 @@ require 'test_plugin_helper'
 class HostTest < ActiveSupport::TestCase
   setup do
     User.current = FactoryBot.build(:user, :admin)
-    setup_settings
     disable_orchestration
     disable_monitoring_orchestration
   end

test/unit/monitoring_result_test.rb
@@ -5,7 +5,6 @@ require 'test_plugin_helper'
 class MonitoringResultTest < ActiveSupport::TestCase
   setup do
     User.current = FactoryBot.build(:user, :admin)
-    setup_settings
     disable_orchestration
     disable_monitoring_orchestration
   end

test/unit/monitoring_test.rb
@@ -5,7 +5,6 @@ require 'test_plugin_helper'
 class MonitoringTest < ActiveSupport::TestCase
   setup do
     User.current = FactoryBot.build(:user, :admin)
-    setup_settings
     disable_monitoring_orchestration
   end
 

commit 61b3d775eed7cf97cc206e4d0a666d645b7a59e0

use a `proc {}` for collections in settings
this was forgotten when migrating from old settings, but without it you
get errors like

    undefined method `call' for {"none"=>"None", "create"=>"Create Host Object"}:Hash

Fixes: 021ecc26654d2a7d21a8a60ebd9ff53b7db15c26

lib/foreman_monitoring/engine.rb
@@ -34,13 +34,13 @@ module ForemanMonitoring
                     description: N_('What action should be taken when a host is created'),
                     default: 'create',
                     full_name: N_('Host Create Action'),
-                    collection: ::Monitoring::CREATE_ACTIONS)
+                    collection: proc { ::Monitoring::CREATE_ACTIONS })
             setting('monitoring_delete_action',
                     type: :string,
                     description: N_('What action should be taken when a host is deleted'),
                     default: 'delete',
                     full_name: N_('Host Delete Action'),
-                    collection: ::Monitoring::DELETE_ACTIONS)
+                    collection: proc { ::Monitoring::DELETE_ACTIONS })
           end
         end
 