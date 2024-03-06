0c19c4d963107c9be7515d71f9a8e80aca1b7111
akumari <akumari@redhat.com>
2024-03-06 12:15:50 +0530

    inherit theforeman-rubocop config

--- db/migrate/201910180900_rename_downtime_host_permission.rb
+++ db/migrate/201910180900_rename_downtime_host_permission.rb
@@ -2,14 +2,10 @@
 
 class RenameDowntimeHostPermission < ActiveRecord::Migration[5.2]
   def up
-    # rubocop:disable Rails/SkipsModelValidations
     Permission.where(name: 'manage_host_downtimes').update_all(name: 'manage_downtime_hosts') if table_exists?(:permissions)
-    # rubocop:enable Rails/SkipsModelValidations
   end
 
   def down
-    # rubocop:disable Rails/SkipsModelValidations
     Permission.where(name: 'manage_downtime_hosts').update_all(name: 'manage_host_downtimes') if table_exists?(:permissions)
-    # rubocop:enable Rails/SkipsModelValidations
   end
 end
--- db/migrate/20221026065954_fix_monitoring_settings_category_to_dsl.rb
+++ db/migrate/20221026065954_fix_monitoring_settings_category_to_dsl.rb
@@ -2,8 +2,6 @@
 
 class FixMonitoringSettingsCategoryToDsl < ActiveRecord::Migration[6.0]
   def up
-    # rubocop:disable Rails/SkipsModelValidations
     Setting.where(category: 'Setting::Monitoring').update_all(category: 'Setting') if column_exists?(:settings, :category)
-    # rubocop:enable Rails/SkipsModelValidations
   end
 end
